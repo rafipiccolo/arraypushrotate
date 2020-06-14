99b8990f4a01978c694f89bac448e742efc14992
root <root@raphaelpiccolo.com>
2020-06-14 12:57:50 +0200

    x

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = function (array, data, maxsize) {
-    var res = null;
     array.push(data);
+
     if (array.length > maxsize)
-        res = array.shift();
-    return res;
+        return array.shift();
+
+    return null;
 };
