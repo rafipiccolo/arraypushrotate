df1c43bfc66ff23030170f14dd45fd4e5a503021
root <rafi.piccolo@gmail.com>
2021-03-23 13:50:04 +0100

    let

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
-var arraypushrotate = require('./index.js');
-var assert = require('assert');
+let arraypushrotate = require('./index.js');
+let assert = require('assert');
 
 describe('arraypushrotate', function () {
-    var array = [];
+    let array = [];
 
     it('should push and not remove', function () {
-        var res = arraypushrotate(array, 'test1', 2);
+        let res = arraypushrotate(array, 'test1', 2);
         assert.equal(res, null);
         assert.deepEqual(array, ['test1']);
     });
 
     it('should push and not remove', function () {
-        var res = arraypushrotate(array, 'test2', 2);
+        let res = arraypushrotate(array, 'test2', 2);
         assert.equal(res, null);
         assert.deepEqual(array, ['test1', 'test2']);
     });
 
     it('should push and remove', function () {
-        var res = arraypushrotate(array, 'test3', 2);
+        let res = arraypushrotate(array, 'test3', 2);
         assert.equal(res, 'test1');
         assert.deepEqual(array, ['test2', 'test3']);
     });
