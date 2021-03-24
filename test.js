e9578ae8986f9c2181c35b4d5d7f8dbdf99fbfdd
root <rafi.piccolo@gmail.com>
2021-03-24 11:12:25 +0100

    eslint

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 let arraypushrotate = require('./index.js');
 let assert = require('assert');
 
-describe('arraypushrotate', function () {
+describe('arraypushrotate', () => {
     let array = [];
 
-    it('should push and not remove', function () {
+    it('should push and not remove', () => {
         let res = arraypushrotate(array, 'test1', 2);
         assert.equal(res, null);
         assert.deepEqual(array, ['test1']);
     });
 
-    it('should push and not remove', function () {
+    it('should push and not remove', () => {
         let res = arraypushrotate(array, 'test2', 2);
         assert.equal(res, null);
         assert.deepEqual(array, ['test1', 'test2']);
     });
 
-    it('should push and remove', function () {
+    it('should push and remove', () => {
         let res = arraypushrotate(array, 'test3', 2);
         assert.equal(res, 'test1');
         assert.deepEqual(array, ['test2', 'test3']);
